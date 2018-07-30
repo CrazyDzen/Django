e3024222d9f4fdd2e7a9dba86da3bb33b09a07b2
CrazyDzen <ovevan89@gmail.com>
2018-07-30 12:36:55 +0300

    lessons_4

--- toys_shop/server/products/urls.py
+++ toys_shop/server/products/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('group<int:pk>/', views.category_list, name='group'),
+    path('good<int:pk>/', views.description, name='description'),
+    path('toys/', views.toys, name='toys'),
+    path('admin/', views.admin, name='admin'),
+    path('edit/', views., name='edit')
 ]
--- toys_shop/server/products/views.py
+++ toys_shop/server/products/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render , get_list_or_404, get_object_or_404
+from django.views.generic import ListView, DetailView
 from . import models
 import categories
 
 
 def index(request):
-    product = models.Product.objects.all()
-    category = categories.models.Category.objects.all()
+    category = get_list_or_404(categories.models.Category)
+    product = get_list_or_404(models.Product)
     return render(request, 'products/index.html', {'product': product, 'category': category})
+
+
+def category_list(request, pk):
+    category = get_list_or_404(categories.models.Category)
+    group = get_object_or_404(categories.models.Category, id=pk)
+    product = get_list_or_404(models.Product, category_id=pk)
+    return render(request, 'products/group.html', {'category': category, 'product': product, 'group': group})
+
+
+def description(request, pk):
+    category = get_list_or_404(categories.models.Category)
+    product = get_object_or_404(models.Product, id=pk)
+    return render(request, 'products/description.html', {'category': category, 'product': product})
+
+
+def toys(request):
+    category = get_list_or_404(categories.models.Category)
+    product = get_list_or_404(models.Product)
+    return render(request, 'products/products.html', {'product': product, 'category': category})
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = models.Product
+        fields = ['name', 'image', 'content', 'category', 'cost']
+
